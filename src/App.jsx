e13a6addae010c0051ba551af4ce0478bcc3530d
Guilherme Alves <guilhermealvescoc@gmail.com>
2023-06-05 15:03:35 -0300

    Build

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-red-300">
+      <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
